2fcf89da8d8a8bb92fe3e68d0d7a291823e2051d
Matthew Yates <Mat.C.Yates@gmail.com>
2019-06-30 15:43:37 -0400

    implemented base controller class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-import * as express from 'express';
+import express from 'express';
 import { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+
+const app = express();
+app.use(bodyParser.json());
+
+// routes
+
+app.get('/users', function(req: Request, res: Response) {
+	// here we will have logic to return all users
+});
+
+app.get('/users/:id', function(req: Request, res: Response) {
+	// here we will have logic to return user by id
+});
+
+app.post('/users', function(req: Request, res: Response) {
+	// here we will have logic to save a user
+});
+
+app.put('/users/:id', function(req: Request, res: Response) {
+	// here we will have logic to update a user by a given user id
+});
+
+app.delete('/users/:id', function(req: Request, res: Response) {
+	// here we will have logic to delete a user by a given user id
+});
+
+// start express server
+app.listen(3000);
 
 createConnection()
 	.then(async (connection) => {})
