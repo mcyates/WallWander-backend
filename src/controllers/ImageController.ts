c21c1e41c51794d962f5dcd263fbb3276fa62c24
m-c-yates <mat.c.yates@gmail.com>
2019-08-05 14:11:46 +0000

    .

---
+++ src/controllers/ImageController.ts
@@ -0,0 +1,108 @@
+import jwt from 'jsonwebtoken';
+import express, { Request, Response, NextFunction } from 'express';
+import multer from 'multer';
+import uuid from 'uuid';
+// @ts-ignore
+import setUpPaginator from 'knex-paginator';
+
+import { Authenticate } from '../middleware/auth';
+import { db } from '../database/database';
+import imgUpload from '../cloudinary';
+
+setUpPaginator( db );
+
+const upload = multer( {
+	dest: './uploads',
+	limits: {
+		fileSize: 10000000
+	},
+	fileFilter ( req, file, cb ) {
+		const isImage = /\.(?:jpg|jpeg|gif|png|webP)/g;
+		if ( isImage.test( file.originalname.toLowerCase() ) === false ) {
+			return cb( new Error( 'file must be a image' ), false );
+		}
+
+		cb( null, true );
+	}
+} );
+
+export const router = express.Router();
+
+// get all images
+router.get( `/images`, async ( req: Request, res: Response ) => {
+	if ( req.query.limit && req.query.page ) {
+		const limit = req.query.limit || 5;
+		const page = parseFloat( req.query.page ) + 1 || 1;
+		const images = await db
+			.select( '*' )
+			.from( 'images' )
+			// @ts-ignore
+			.paginate( limit, page, true );
+		return res.json( images );
+	}
+	return res.status( 400 ).json( 'missing query parameters' );
+} );
+
+// get image by id
+router.get( `/images/:id`, async ( req: Request, res: Response ) => {
+	const { id } = req.params;
+	const image = await db
+		.select( '*' )
+		.from( 'images' )
+		.where( { id } );
+	await db( 'images' )
+		.where( { id } )
+		.increment( 'views', 1 )
+		.catch( ( e ) => console.log( e ) );
+	return res.json( image );
+} );
+
+// upload new image
+router.post(
+	`/images/upload`,
+	Authenticate,
+	upload.single( 'wallpaper' ),
+	async ( req: any, res: Response ) => {
+		const { authorization } = req.headers;
+		let authorId = await jwt.verify( authorization, `${ process.env.SECRET }` );
+		const id = await uuid.v4();
+
+		await imgUpload( req.file ).then( ( image ) => {
+			const { url, secureUrl, width, height, format, title } = image;
+			db( 'images' )
+				.insert( {
+					id,
+					url,
+					secureUrl,
+					title,
+					width,
+					height,
+					format,
+					views: 0,
+					authorId,
+					authorToken: authorization
+				} )
+				.returning( '*' )
+				.then( async ( image ) => {
+					const img = image[ 0 ];
+					res.status( 201 ).json( img );
+				} );
+		} );
+	},
+	( error: Error, req: Request, res: Response, next: NextFunction ) => {
+		res.sendStatus( 400 ).json( { error: error.message } );
+	}
+);
+
+// delete image by id
+router.delete( `/images/:id`, async ( req: Request, res: Response ) => {
+	const { id } = req.params;
+
+	const image = await db( 'images' ).where( { id } );
+	await db( 'images' )
+		.where( { id } )
+		.del();
+	return res.json( image[ 0 ] );
+} );
+
+export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Authenticate } from './middleware/auth';
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import helmet = require('helmet');
+import helmet = require( 'helmet' );
 import cors from 'cors';
 
-import userRouter from './controllers/userController';
-import imageRouter from './controllers/imageController';
+import userRouter from './controllers/UserController';
+import imageRouter from './controllers/ImageController';
 import { initDb } from './database/database';
 
 initDb();
@@ -13,35 +13,35 @@ initDb();
 const app = express();
 const port = process.env.PORT || 4000;
 
-const whitelist = ['mattcyates.com', 'www.mattcyates.com'];
+const whitelist = [ 'mattcyates.com', 'www.mattcyates.com' ];
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+app.use( function ( req, res, next ) {
+	res.header( 'Access-Control-Allow-Origin', '*' );
+	res.header( 'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS' );
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, x-auth'
 	);
-	res.header('Access-Control-Expose-Headers', 'x-auth');
+	res.header( 'Access-Control-Expose-Headers', 'x-auth' );
 	next();
-});
+} );
 
 const corsOptions = {
-	exposedHeaders: ['authorization']
+	exposedHeaders: [ 'authorization' ]
 };
 
-app.use(cors(corsOptions));
-app.use(helmet());
+app.use( cors( corsOptions ) );
+app.use( helmet() );
 
 // app.use(Authenticate);
-app.use(bodyParser.json());
-app.use(userRouter);
-app.use(imageRouter);
+app.use( bodyParser.json() );
+app.use( userRouter );
+app.use( imageRouter );
 
-app.get('/', (req: Request, res: Response) => {
-	return res.status(200).json('hello');
-});
+app.get( '/', ( req: Request, res: Response ) => {
+	return res.status( 200 ).json( 'hello' );
+} );
 
-app.listen(port, () => {
-	console.log(`server running at port ${port}`);
-});
+app.listen( port, () => {
+	console.log( `server running at port ${ port }` );
+} );
