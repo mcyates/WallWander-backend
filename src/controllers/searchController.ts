dea912c837320b1977163c45fb02a942fcafc6be
Matthew Yates <Mat.C.Yates@gmail.com>
2019-08-23 15:50:22 -0400

    tags

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -11,6 +11,8 @@ export const router = express.Router();
 
 router.get('/search/', async (req: Request, res: Response) => {
 	let { tags, nsfw } = req.query;
+	const limit = req.query.limt || 5;
+	const page = parseFloat(req.query.page) + 1 || 1;
 
 	tags = tags.split('+');
 
@@ -27,18 +29,25 @@ router.get('/search/', async (req: Request, res: Response) => {
 
 	if (tagExists) {
 		await db('tags')
-			.select('id')
+			.select(['id', 'tag'])
 			.whereIn('tag', [...tags])
 			.then(async (data) => {
 				const ids = data.map(({ id }) => id);
+				const tags = data.map(({ tag }) => tag);
+
 				const imgs = await db
 					.select(['imageId as id', 'secureUrl', 'height', 'width', 'views'])
 					.from('images')
 					.innerJoin('images_tags', 'images.id', 'images_tags.imageId')
-					.whereIn('images_tags.tagId', [...ids]);
+					.whereIn('images_tags.tagId', [...ids])
+					// @ts-ignore
+					.paginate(limit, page, true);
 				// .catch((e) => res.status(404).json(e.datail));
-				console.log(imgs);
-				res.json(imgs);
+				const result = {
+					imgs,
+					tags
+				};
+				res.json(result);
 			})
 			.catch((e) => res.status(400).json(e));
 	} else {
