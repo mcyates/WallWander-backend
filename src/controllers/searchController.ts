eba5727b42b806925f90182b4c44da8826644f7d
Matthew Yates <Mat.C.Yates@gmail.com>
2019-08-24 15:51:40 -0400

    user search prototype completed

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -14,6 +14,12 @@ router.get('/search/', async (req: Request, res: Response) => {
 	const limit = req.query.limt || 5;
 	const page = parseFloat(req.query.page) + 1 || 1;
 
+	if (nsfw === 'true') {
+		nsfw = true;
+	} else if ((nsfw = 'false')) {
+		nsfw = false;
+	}
+
 	tags = tags.split('+');
 
 	const tagExists = await db('tags')
@@ -23,14 +29,21 @@ router.get('/search/', async (req: Request, res: Response) => {
 			if (rows.length === 0) {
 				return false;
 			}
+			tags = rows;
+			if (nsfw === false) {
+				tags = tags.filter((tag: any) => {
+					return tag.nsfw === false;
+				});
+			}
 			return true;
 		})
 		.catch((e) => res.status(400).json(e.detail));
-
+	console.log(tags);
+	const queryTags = tags.map(({ tag }: any) => tag);
 	if (tagExists) {
 		await db('tags')
 			.select(['id', 'tag'])
-			.whereIn('tag', [...tags])
+			.whereIn('tag', [...queryTags])
 			.then(async (data) => {
 				const ids = data.map(({ id }) => id);
 				const tags = data.map(({ tag }) => tag);
@@ -41,8 +54,8 @@ router.get('/search/', async (req: Request, res: Response) => {
 					.innerJoin('images_tags', 'images.id', 'images_tags.imageId')
 					.whereIn('images_tags.tagId', [...ids])
 					// @ts-ignore
-					.paginate(limit, page, true);
-				// .catch((e) => res.status(404).json(e.datail));
+					.paginate(limit, page, true)
+					.catch((e: any) => res.status(404).json(e.datail));
 				const result = {
 					imgs,
 					tags
@@ -50,8 +63,6 @@ router.get('/search/', async (req: Request, res: Response) => {
 				res.json(result);
 			})
 			.catch((e) => res.status(400).json(e));
-	} else {
-		res.status(404).json('tags not found');
 	}
 });
 
